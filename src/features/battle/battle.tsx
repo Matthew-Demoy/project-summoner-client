512624f2177a9f11bb8593f91bbb5ab7196c3fa8
Matthew Demoy <MatthewDemoy@student.uml.edu>
2021-10-30 11:57:15 -0400

    fix summoner info retrieval

--- src/features/battle/battle.tsx
+++ src/features/battle/battle.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  getAllCooldowns,
+  getAllPositions,
+  getAllSummonerInfo,
+} from "./battleSlice";
 import Cooldowns from "./cooldowns/Cooldowns";
 import Board from "./map/Board";
 
 const Battle = () => {
-    
- 
+  const dispatch = useAppDispatch();
+  const { teamOne, teamTwo } = useAppSelector((s) => s.battle);
+  useEffect(() => {
+    dispatch(getAllCooldowns({ teamOne, teamTwo }));
+    dispatch(getAllSummonerInfo({ teamOne, teamTwo }));
+    dispatch(getAllPositions({ teamOne, teamTwo }));
+  }, []);
+
   return (
     <div>
-    <Cooldowns/>
+      <Cooldowns />
       <Board />
       {/**
        * Map
